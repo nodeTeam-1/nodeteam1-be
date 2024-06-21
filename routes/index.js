ac88065ee3dfa20fccde424953403790f6824df6
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2024-06-21 16:32:53 +0900

    팔로우 언팔로우 완성

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const authApi = require('./auth.api');
 const userApi = require('./user.api');
+const followApi = require('./follow.api');
 
 router.use('/auth', authApi);
 router.use('/user', userApi);
+router.use('/follow', followApi);
 
 module.exports = router;
