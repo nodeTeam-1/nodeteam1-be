2855bb463df2b202a10c57335f3950030848c941
LAPTOP-9618V2DO\POINT-I <agratos318@naver.com>
2024-06-21 15:38:30 +0900

    user register, login, info 완료

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,7 +16,7 @@ userController.createUser = async (req, res) => {
 
     // 비밀번호 암호화
     const salt = bcrypt.genSaltSync(10);
-    password = await bcrypt.hashSync(password, salt);
+    password = bcrypt.hashSync(password, salt);
 
     // 유저 정보 객체에 담아 저장
     const newUser = new User({ email, password, name, level: level ? level : 'public' }); // public || influencers
@@ -25,6 +25,19 @@ userController.createUser = async (req, res) => {
     return res.status(200).json({ status: 'success' });
   } catch (err) {
     res.status(500).json({ status: 'fail', error: err, message: err.message });
+  }
+};
+
+userController.getUser = async (req, res) => {
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
+    if (user) {
+      return res.status(200).json({ status: 'success', user });
+    }
+    throw new Error('토큰이 유효하지 않습니다.');
+  } catch (error) {
+    res.status(400).json({ status: 'fail', error: error.message });
   }
 };
 
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+
+const authController = require('../controllers/auth.controller');
 const userController = require('../controllers/user.controller');
 
 // 회원가입
-router.post('/', userController.createUser);
+router.post('/register', userController.createUser);
+
+// 유저 정보
+router.get('/info', authController.authenticate, userController.getUser);
+
+//TODO - 비밀번호 변경
+
+//TODO - 유저 정보 변경 (프로필, 인사말)
 
 module.exports = router;
