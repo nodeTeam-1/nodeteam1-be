0f1fa4af154d7369fabeb7704498d675fb937da0
DESKTOP-DM6TCSG\LeeJaeYong <agratos318@naver.com>
2024-06-21 20:49:28 +0900

    스키마 변경

--- models/Post.js
+++ models/Post.js
@@ -7,8 +7,9 @@ const postSchema = new Schema(
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, required: true },
     content: { type: String, required: true },
-    images: { type: [String] },
+    images: { type: [String], default: [] },
     category: { type: String, required: true },
+    tags: { type: [String], default: [] },
     likeCount: { type: Number, default: 0 }
   },
   {
