b9f54bb17e382ef979780618f7872780fa9bc55d
claire <61533589+joooo24@users.noreply.github.com>
2024-06-29 20:35:10 +0900

    feat: 포스트 아이디별 조회 추가

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -3,15 +3,15 @@ const User = require('../models/User');
 
 const postController = {};
 
+// 포스트 전체 가져오기
 postController.getPosts = async (req, res) => {
   try {
     const { page, name, pageSize = 1 } = req.query;
-
     const condition = {
       ...(name && { name: { $regex: name, $options: 'i' } })
     };
 
-    let query = Post.find(condition).populate('userId', 'name');
+    let query = Post.find(condition).populate('userId', 'name').sort({ createdAt: -1 });
     let response = { status: 'success' };
 
     if (page) {
@@ -34,101 +34,101 @@ postController.getPosts = async (req, res) => {
   }
 };
 
+// 포스트 ID별 가져오기
+postController.getPostsId = async (req, res) => {
+  try {
+    const postId = req.params.id; // 파라미터에서 postId 가져옴
+    const post = await Post.findById(postId).populate('userId', 'name'); // postId로 포스트 조회
+
+    if (!post) {
+      return res.status(404).json({ status: 'fail', message: 'Post not found' });
+    }
+
+    return res.status(200).json({ status: 'success', data: post });
+  } catch (error) {
+    return res.status(400).json({ status: 'fail', error: error.message });
+  }
+};
+
+// 포스트 상세 가져오기
 postController.getPostDetail = async (req, res) => {
   try {
     const postId = req.params.id;
-
     const post = await Post.findOne({ _id: postId }).populate('userId', 'name');
-
     return res.status(200).json({ status: 'success', post });
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
 };
 
+// 포스트 생성
 postController.createPost = async (req, res) => {
   try {
     const { userId } = req;
     const { title, content, images, category, tags } = req.body;
-
     const post = new Post({ userId, title, content, images, category, tags });
     await post.save();
-
     return res.status(200).json({ status: 'success', post });
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
 };
 
+// 포스트 업데이트
 postController.updatePost = async (req, res) => {
   try {
     const { userId } = req;
     const { postId, title, content, images, category, tags } = req.body;
-
     const post = await Post.findById(postId);
     if (!post) throw new Error('Post not found');
-
     if (!post.userId.equals(userId)) throw new Error('Unauthorized: You can only edit your own posts');
-
     Object.assign(post, { title, content, images, category, tags });
     await post.save();
-
     return res.status(200).json({ status: 'success', post });
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
 };
 
+// 포스트 삭제
 postController.deletePost = async (req, res) => {
   try {
     const { userId } = req;
     const postId = req.params.id;
-
     const post = await Post.findById(postId);
     if (!post) throw new Error('Post not found');
-
     if (!post.userId.equals(userId)) throw new Error('Unauthorized: You can only edit your own posts');
-
     await Post.deleteOne({ _id: postId });
-
     return res.status(200).json({ status: 'success' });
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
 };
 
+// 포스트 좋아요 생성
 postController.createPostLike = async (req, res) => {
   try {
     const postId = req.params.id;
     const { userId } = req;
-
     const user = await User.findById({ _id: userId });
-
     if (user.postLike.includes(postId)) throw new Error('Post already liked by user');
-
     await User.findByIdAndUpdate({ _id: userId }, { $addToSet: { postLike: postId } }, { new: true });
-
     await Post.findOneAndUpdate({ _id: postId }, { $inc: { likeCount: 1 } }, { new: true });
-
     return res.status(200).json({ status: 'success' });
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
   }
 };
 
+// 포스트 좋아요 삭제
 postController.deletePostLike = async (req, res) => {
   try {
     const postId = req.params.id;
     const { userId } = req;
-
     const user = await User.findById({ _id: userId });
-
     if (!user.postLike.includes(postId)) throw new Error('Post not liked by user');
-
     await User.findByIdAndUpdate({ _id: userId }, { $pull: { postLike: postId } }, { new: true });
-
     await Post.findOneAndUpdate({ _id: postId }, { $inc: { likeCount: -1 } }, { new: true });
-
     return res.status(200).json({ status: 'success' });
   } catch (error) {
     res.status(400).json({ status: 'fail', error: error.message });
